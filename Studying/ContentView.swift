2cf4159df9cd67a3f3fcbfe43f470cac48dcd282
Concerto <zxysb0@qq.com>
2024-05-06 22:59:10 +0800

    ForEach

--- Studying/ContentView.swift
+++ Studying/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["🥰","😜","🤪","🥳","🤩"]
     var body: some View {
         VStack(spacing: 10,content: {
-            CardView(faceUp: true)
-            CardView(faceUp: true)
-            CardView()
-            CardView()
+            ForEach(emojis.indices,id:\.self){index in
+                CardView(content: emojis[index])
+            }
         })
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct CardView: View {
+    
+    var content:String
+    @State var faceUp:Bool = false
+    
+    
+    var body: some View {
+        ZStack{
+            if faceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 10,dash: [10,2]))
+                    .foregroundColor(.orange)
+                Text(content)
+                    .font(Font.system(size: 40))
+            }else{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.orange)
+            }
+        }
+        .onTapGesture(count: 1, perform: {
+            print("tapped")
+            faceUp.toggle()
+            
+        })
+    }
+}
+
+struct ContentView_Preview:PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
+}
+struct CardView_Preview:PreviewProvider{
+    static var previews: some View{
+        CardView(content: "🥲")
+    }
 }
