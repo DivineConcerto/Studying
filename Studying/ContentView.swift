cab1fee7fe7565792f7201f1daf5d0af457978d8
Concerto <zxysb0@qq.com>
2024-05-07 10:34:19 +0800

    Third

--- Studying/ContentView.swift
+++ Studying/ContentView.swift
@@ -9,37 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojis = ["🥰","😜","🤪","🥳","🤩"]
+    let emojis = ["🥰","😜","🤪","🥳","🤩","😇","😅"]
+    @State var showAmount:Int = 4
     var body: some View {
-        VStack(spacing: 10,content: {
-            ForEach(emojis.indices,id:\.self){index in
+        VStack{
+            cards
+            cardsAdjuster
+        }
+        .padding()
+    }
+    
+    
+    func cardCountAdjuster(by offset:Int,symbol:String) -> some View{
+        Button(action: {
+            showAmount += offset
+        }, label: {
+            Image(systemName: symbol)
+                .imageScale(.large)
+                .font(.largeTitle)
+        })
+        .disabled(showAmount + offset > emojis.count || showAmount + offset < 1)
+    }
+        
+    
+    var cardsAdjuster:some View{
+        HStack{
+            cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+            Spacer()
+            cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+        }
+        
+    }
+    
+    var cards:some View{
+        LazyVGrid(columns: [GridItem(),GridItem()], content: {
+            ForEach(0..<showAmount,id:\.self){index in
                 CardView(content: emojis[index])
             }
+            
         })
-        .padding()
+       
+        .foregroundColor(.orange)
+    }
+    
+    
+    
+    var addButton:some View{
+        Button(action: {
+            if showAmount < emojis.count{
+                showAmount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        })
+    }
+    
+    var minusButton:some View{
+        Button(action: {
+            if showAmount > 1{
+                showAmount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        })
     }
 }
 
 struct CardView: View {
     
     var content:String
-    @State var faceUp:Bool = false
+    @State var faceUp:Bool = true
     
     
     var body: some View {
         ZStack{
-            if faceUp{
+            RoundedRectangle(cornerRadius: 12)
+            Group{
                 RoundedRectangle(cornerRadius: 12)
                     .fill(.white)
                 RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(style: StrokeStyle(lineWidth: 10,dash: [10,2]))
-                    .foregroundColor(.orange)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 5,dash: [10,2]))
                 Text(content)
                     .font(Font.system(size: 40))
-            }else{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.orange)
             }
+            .opacity(faceUp ? 1 : 0)
         }
         .onTapGesture(count: 1, perform: {
             print("tapped")
